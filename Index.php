8f4f9413cfb664c97043f51adec8a9f2c20e81b4
yandiaan <diansetiawan2121@gmail.com>
2021-10-19 14:51:59 +0700

    t

--- Index.php
+++ Index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Made Arya Putra Wibawa(205410065)</h3>"
-echo "<h3>Dian Setiawan(205410048)</h3>"
+echo "<h3>Ng. Made Arya Putra Wibawa(205410065)</h3>";
+echo "<h3>Dian Setiawan(205410048)</h3>";
 
 ?>
